5690ebf5c96e8253f6eb45a427d5c12459a11554
Mikhail Gerasimov <msgerasimov_1@edu.hse.ru>
2023-05-16 01:47:31 +0300

    Moved AnimatedSprite class to a separate file

--- the_game_itself.py
+++ the_game_itself.py
@@ -3,6 +3,7 @@ import random
 from cursor import Cursor
 from bullet import Bullet
 from player_spaceship import PlayerSpaceship
+from animated_sprite import AnimatedSprite
 
 pygame.init()
 pygame.mouse.set_visible(False)
@@ -179,29 +180,7 @@ class RivalSpaceship(pygame.sprite.Sprite):
                 health -= 1
 
 
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, group, sheet, columns, rows, x, y):
-        super().__init__(group)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
 
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
-        if self.cur_frame == len(self.frames) - 1:
-            self.kill()
 
 
 def start_animation():
